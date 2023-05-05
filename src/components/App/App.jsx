f6af99e376073b320f34e14757f979957b4eaa52
GudLub <gudilinalu@yandex.ru>
2023-05-05 21:41:44 +0300

    step 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import AppHeader from '../AppHeader/AppHeader.jsx'
-import Main from '../Main/Main.jsx'
-import styles from './App.module.scss'
+import { useState, useEffect } from "react";
+import AppHeader from "../AppHeader/AppHeader.jsx";
+import Main from "../Main/Main.jsx";
+import styles from "./App.module.scss";
 
 function App() {
+  const serverUrl = "https://norma.nomoreparties.space/api/ingredients";
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      return await fetch(serverUrl)
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(`Ошибка ${res.status}`);
+        })
+        .then((data) => setState(data.data))
+        .catch((err) => console.log(err));
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className={styles.app}>
       <AppHeader />
-      <Main />
+      <Main data={state} />
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,20 @@
-import styles from './Main.module.scss';
-import BurgerIngredients from '../BurgerIngredients/BurgerIngredients.jsx';
-import BurgerConstructor from '../BurgerConstructor/BurgerConstructor.jsx';
+import styles from "./Main.module.scss";
+import BurgerIngredients from "../BurgerIngredients/BurgerIngredients.jsx";
+import BurgerConstructor from "../BurgerConstructor/BurgerConstructor.jsx";
+import { ingredientPropTypes } from "../../utils/PropTypes";
+import PropTypes from "prop-types";
 
+const Main = ({ data }) => {
+  return (
+    <main className={styles.main}>
+      <BurgerIngredients data={data} />
+      <BurgerConstructor data={data} />
+    </main>
+  );
+};
 
-const Main = () => {
-    return (
-        <main className={styles.main}>
-            <BurgerIngredients />
-            <BurgerConstructor />
-        </main>
-    )
-}
+Main.propTypes = {
+  data: PropTypes.arrayOf(ingredientPropTypes).isRequired,
+};
+
 export default Main;
